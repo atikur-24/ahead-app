2bce15fe5893c8dd37e176e0820258169379d4e0
Atikur <atikurrahman7223@gmail.com>
2023-10-29 11:50:35 +0600

    added eq section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Banner from "@/components/Banner";
+import Eq from "@/components/Eq";
 
 const HomePage = () => {
   return (
     <main>
       <Banner />
+      <Eq />
     </main>
   );
 };
---
+++ src/components/Eq.jsx
@@ -0,0 +1,13 @@
+const Eq = () => {
+  return (
+    <section className="my-container">
+      <div className="px-10 lg:px-16 flex gap-5">
+        <h1 className="text-lg md:text-2xl lg:text-3xl tracking-wide font-semibold lg:font-bold mr-36 whitespace-nowrap">EQ beats IQ</h1>
+        <p className="leading-7">People with high emotional intelligence (EQ) live more fulfilled lives. They tend to be happier and have healthier relationships.</p>
+        <p className="leading-7">They are more successful in their pursuits and make for inspiring leaders. According to science, they earn $29k a year.</p>
+      </div>
+    </section>
+  );
+};
+
+export default Eq;
