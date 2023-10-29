f5457bdb6ac60504f1a4b68a6d42904d42017e74
Atikur <atikurrahman7223@gmail.com>
2023-10-30 00:16:11 +0600

    added improve steps bar

--- src/components/Improvement.jsx
+++ src/components/Improvement.jsx
@@ -1,4 +1,28 @@
-import "@/styles/style.css";
+import Box from "@mui/material/Box";
+import Step from "@mui/material/Step";
+import StepContent from "@mui/material/StepContent";
+import StepLabel from "@mui/material/StepLabel";
+import Stepper from "@mui/material/Stepper";
+import Typography from "@mui/material/Typography";
+
+const steps = [
+  {
+    label: "It's not as easy as 1-2-3.",
+    description: "The journey of change may be long, but our sessions are quick. We get to the point and tell you what you want to know (and nothing else).",
+  },
+  {
+    label: "Old habits are hard to break.",
+    description: "And bad behaviors die hard. Fortunately, we give you great, science- backed techniques to use",
+  },
+  {
+    label: "You and your motivation don't have a long-term relationship.",
+    description: "Luckily, we can proactively prepare you for the marathon, not just the race. Effective, memorable exerciseswill help you stick to your goals.",
+  },
+  {
+    label: "Books just don't offer practical solutions.",
+    description: "Remember when vou leamed to ride a hike iust hv reading? Yeah. we don't either",
+  },
+];
 
 const Improvement = () => {
   return (
@@ -8,12 +32,16 @@ const Improvement = () => {
         <h2 className="text-lg md:text-3xl lg:text-4xl tracking-wide font-semibold lg:font-bold">Self-improvement. Ugh.</h2>
       </div>
       <div className="mx-40">
-        <ul className="steps steps-vertical">
-          <li className="step step-primary">Register</li>
-          <li className="step step-primary">Choose plan</li>
-          <li className="step step-primary">Purchase</li>
-          <li className="step step-primary">Receive Product</li>
-        </ul>
+        <Box sx={{ maxWidth: 400 }}>
+          <Stepper activeStep={true} orientation="vertical">
+            {steps.map((step) => (
+              <Step key={step.label}>
+                <StepLabel>{step.label}</StepLabel>
+                <StepContent>{step.description}</StepContent>
+              </Step>
+            ))}
+          </Stepper>
+        </Box>
       </div>
     </section>
   );
