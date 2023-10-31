f7ae3a7ca153cfe887409d1ddf8087fd5172a942
Atikur <atikurrahman7223@gmail.com>
2023-10-31 12:20:20 +0600

    fixed padding in wondered

--- src/components/Wondered.jsx
+++ src/components/Wondered.jsx
@@ -4,7 +4,7 @@ import Step from "./Step";
 const Wondered = () => {
   return (
     <section className="my-container">
-      <div className="bg-[#EDF8FE] rounded-xl lg:rounded-3xl p-5 lg:p-10 text-center">
+      <div className="bg-[#EDF8FE] rounded-xl lg:rounded-3xl p-8 lg:py-20 lg:px-10 text-center">
         <p className="font-medium pb-1 lg:pb-2">Let your friends, family, and co-workers (anonymously) rate your social skills.</p>
         <h2 className="text-lg md:text-3xl lg:text-4xl tracking-wide font-semibold lg:font-bold pb-6 lg:pb-10">Ever wondered what others think of you?</h2>
         {/* <Step /> */}
